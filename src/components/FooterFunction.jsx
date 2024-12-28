5eebe6d40f1c88052c90162a993483e6c4e3680a
Ömer Mercan <70708913+O-Mercan@users.noreply.github.com>
2024-12-28 12:19:10 +0300

    Reactjs styling added

--- src/components/FooterFunction.jsx
+++ src/components/FooterFunction.jsx
@@ -3,44 +3,93 @@ import React from 'react'
 export default function FooterFunction() {
   return (
     <div>
-      <section className="">
-  {/* Footer */}
-  <footer
-    className="text-center text-white fized-bot"
-    style={{ backgroundColor: "#0a4275" }}
-  >
-    {/* Grid container */}
-    <div className="container p-4 pb-0">
-      {/* Section: CTA */}
-      <section className="">
-        <p className="d-flex justify-content-center align-items-center">
-          <span className="me-3">Register for free</span>
-          <button
-            data-mdb-ripple-init=""
-            type="button"
-            className="btn btn-outline-light btn-rounded"
-          >
-            Sign up!
-          </button>
-        </p>
-      </section>
-      {/* Section: CTA */}
-    </div>
-    {/* Grid container */}
-    {/* Copyright */}
-    <div
-      className="text-center p-3"
-      style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
-    >
-      © 2020 Copyright:
-      <a className="text-white" href="https://mdbootstrap.com/">
-        MDBootstrap.com
+        <footer className="text-center bg-body-tertiary">
+  {/* Grid container */}
+  <div className="container pt-4">
+    {/* Section: Social media */}
+    <section className="mb-4">
+      {/* Facebook */}
+      <a
+        data-mdb-ripple-init=""
+        className="btn btn-link btn-floating btn-lg text-body m-1"
+        href="#!"
+        role="button"
+        data-mdb-ripple-color="dark"
+      >
+        <i className="fab fa-facebook-f" />
+        Facebook
       </a>
-    </div>
-    {/* Copyright */}
-  </footer>
-  {/* Footer */}
-</section>
+      {/* Twitter */}
+      <a
+        data-mdb-ripple-init=""
+        className="btn btn-link btn-floating btn-lg text-body m-1"
+        href="https://x.com/home?lang=tr"
+        role="button"
+        data-mdb-ripple-color="dark"
+      >
+        <i className="fab fa-twitter" />
+        Twitter
+      </a>
+      {/* Google */}
+      <a
+        data-mdb-ripple-init=""
+        className="btn btn-link btn-floating btn-lg text-body m-1"
+        href="#!"
+        role="button"
+        data-mdb-ripple-color="dark"
+      >
+        <i className="fab fa-google" />
+        Google
+      </a>
+      {/* Instagram */}
+      <a
+        data-mdb-ripple-init=""
+        className="btn btn-link btn-floating btn-lg text-body m-1"
+        href="#!"
+        role="button"
+        data-mdb-ripple-color="dark"
+      >
+        <i className="fab fa-instagram" />
+        Instagram
+      </a>
+      {/* Linkedin */}
+      <a
+        data-mdb-ripple-init=""
+        className="btn btn-link btn-floating btn-lg text-body m-1"
+        href="#!"
+        role="button"
+        data-mdb-ripple-color="dark"
+      >
+        <i className="fab fa-linkedin" />
+        Linkedin
+      </a>
+      {/* Github */}
+      <a
+        data-mdb-ripple-init=""
+        className="btn btn-link btn-floating btn-lg text-body m-1"
+        href="#!"
+        role="button"
+        data-mdb-ripple-color="dark"
+      >
+        <i className="fab fa-github" />
+        Github
+      </a>
+    </section>
+    {/* Section: Social media */}
+  </div>
+  {/* Grid container */}
+  {/* Copyright */}
+  <div
+    className="text-center p-3"
+    style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}
+  >
+    © 2020 Copyright:
+    <a className="text-body" href="https://mdbootstrap.com/">
+      MDBootstrap.com
+    </a>
+  </div>
+  {/* Copyright */}
+</footer>
 
     </div>
   )
--- src/components/HeaderFunction.jsx
+++ src/components/HeaderFunction.jsx
@@ -1,92 +1,86 @@
 import React from 'react'
+import background from '../images/Under The Milky Way.jpg'
+
+import '../style.css'
+
+
 
 
 export default function HeaderFunction() {
+
+  {/* start Header */}
+    {/* Option 1 (Styling) */}
+    <header style= {{color:"blue"}}></header>
+
+    {/* Option 2 (Styling) */}
+    // const headerCss={
+    //   "background-image":"url('../images/Under The Milky Way.jpg')",
+    //   "background-size":"cover",
+    //   "background-positinon":"center",
+    //   "height":"50vh",
+    //   "width":"100%",
+    //   "background-color":"black",
+    //   "color":"white",
+    // }
+
   return (
     <div>
-      <>
-  {/* Navbar */}
-  <nav className="navbar navbar-expand-lg navbar-light bg-body-tertiary">
-    {/* Container wrapper */}
-    <div className="container-fluid">
-      {/* Toggle button */}
-      <button
-        data-mdb-collapse-init=""
-        className="navbar-toggler"
-        type="button"
-        data-mdb-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <i className="fas fa-bars" />
-      </button>
-      {/* Collapsible wrapper */}
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        {/* Navbar brand */}
-        <a className="navbar-brand mt-2 mt-lg-0" href="#">
-          <img
-            src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp"
-            height={15}
-            alt="MDB Logo"
-            loading="lazy"
-          />
-        </a>
-        {/* Left links */}
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Dashboard
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Team
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Projects
-            </a>
-          </li>
-        </ul>
-        {/* Left links */}
-      </div>
-      {/* Collapsible wrapper */}
-      {/* Right elements */}
-      <div className="d-flex align-items-center">
-        {/* Icon */}
-        <a className="text-reset me-3" href="#">
-          <i className="fas fa-shopping-cart" />
-        </a>
-        {/* Notifications */}
-        <div className="dropdown">
+      {/* Nav start */}
+      <nav className="navbar navbar-expand-lg navbar-light bg-body-tertiary">
+  {/* Container wrapper */}
+  <div className="container-fluid">
+    {/* Toggle button */}
+    <button
+      data-mdb-collapse-init=""
+      className="navbar-toggler"
+      type="button"
+      data-mdb-target="#navbarLeftAlignExample"
+      aria-controls="navbarLeftAlignExample"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <i className="fas fa-bars" />
+    </button>
+    {/* Collapsible wrapper */}
+    <div className="collapse navbar-collapse" id="navbarLeftAlignExample">
+      {/* Left links */}
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">
+            Home
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Link
+          </a>
+        </li>
+        {/* Navbar dropdown */}
+        <li className="nav-item dropdown">
           <a
             data-mdb-dropdown-init=""
-            className="text-reset me-3 dropdown-toggle hidden-arrow"
+            className="nav-link dropdown-toggle"
             href="#"
-            id="navbarDropdownMenuLink"
+            id="navbarDropdown"
             role="button"
             aria-expanded="false"
           >
-            <i className="fas fa-bell" />
-            <span className="badge rounded-pill badge-notification bg-danger">
-              1
-            </span>
+            Dropdown
           </a>
-          <ul
-            className="dropdown-menu dropdown-menu-end"
-            aria-labelledby="navbarDropdownMenuLink"
-          >
+          {/* Dropdown menu */}
+          <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
             <li>
               <a className="dropdown-item" href="#">
-                Some news
+                Action
               </a>
             </li>
             <li>
               <a className="dropdown-item" href="#">
-                Another news
+                Another action
               </a>
+            </li>
+            <li>
+              <hr className="dropdown-divider" />
             </li>
             <li>
               <a className="dropdown-item" href="#">
@@ -94,53 +88,38 @@ export default function HeaderFunction() {
               </a>
             </li>
           </ul>
-        </div>
-        {/* Avatar */}
-        <div className="dropdown">
-          <a
-            data-mdb-dropdown-init=""
-            className="dropdown-toggle d-flex align-items-center hidden-arrow"
-            href="#"
-            id="navbarDropdownMenuAvatar"
-            role="button"
-            aria-expanded="false"
-          >
-            <img
-              src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
-              className="rounded-circle"
-              height={25}
-              alt="Black and White Portrait of a Man"
-              loading="lazy"
-            />
-          </a>
-          <ul
-            className="dropdown-menu dropdown-menu-end"
-            aria-labelledby="navbarDropdownMenuAvatar"
-          >
-            <li>
-              <a className="dropdown-item" href="#">
-                My profile
-              </a>
-            </li>
-            <li>
-              <a className="dropdown-item" href="#">
-                Settings
-              </a>
-            </li>
-            <li>
-              <a className="dropdown-item" href="#">
-                Logout
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-      {/* Right elements */}
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+      {/* Left links */}
     </div>
-    {/* Container wrapper */}
-  </nav>
-  {/* Navbar */}
-</>
+    {/* Collapsible wrapper */}
+  </div>
+  {/* Container wrapper */}
+</nav>
+{/* end Nav */}
+
+    {/* start Header */}
+    {/* Option 1 (Styling) */}
+    {/* <header style= {{color:"blue"}}></header> */}
+
+    {/* Option 2 (Styling) */}
+    {/* <header style= {headerCss}>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe voluptate pariatur asperiores corrupti facilis incidunt omnis placeat maiores amet, accusantium nam, temporibus ut mollitia exercitationem illum harum fugit quae voluptatem!
+      Sint, pariatur. Nisi accusantium sunt ullam magni recusandae necessitatibus voluptatum ut voluptas, aliquam sequi nam, odio commodi voluptates. Vel esse impedit modi tenetur mollitia sequi maxime! Quae dolorum beatae adipisci.
+      Tempore ab, nisi numquam excepturi dolorum error labore ullam repellendus. Ipsa, sunt culpa id hic minima repudiandae dolore corporis nisi voluptates! Quas quis id est nemo eum delectus sit animi.
+      Obcaecati autem ipsum optio minus sint incidunt possimus sed veniam est laudantium unde recusandae dolorem modi natus perferendis tempora, molestias ea hic odio in excepturi eaque aperiam dignissimos tempore. Cum.
+    </header>
+    <img src={background} alt="" /> */}
+
+    {/* option 3 */}
+      <header className='backgroundExternal'>
+        <p className='specialParag'>Welcome to React Tutorial</p>
+    </header>
+
+    {/* end Header */}
 
     </div>
   )
